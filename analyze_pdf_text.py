4d9b3f3fe329e254b198cbb834dfb1a8d6702d65
mima3 <7877000+mima3@users.noreply.github.com>
2020-05-07 11:36:36 +0900

    初回コミット

---
+++ analyze_pdf_text.py
@@ -0,0 +1,54 @@
+import sys
+import fitz
+import csv
+import json
+from tqdm import tqdm
+
+
+def main(input_path):
+    argvs = sys.argv
+    argc = len(argvs)
+    if argc != 2:
+        print("Usage #python %s [PDFパス]" % argvs[0])
+        exit()
+    input_path = argvs[1]
+    doc = fitz.open(input_path)
+    list_string = []
+    result = []
+    pno = 0
+    for page in tqdm(doc):
+        wordlist = page.getTextWords()  # gives you a list of all words on the
+        blocklist = page.getTextBlocks()
+        for block in blocklist:
+            r = fitz.Rect(block[0], block[1], block[2], block[3])
+            txt = block[4].replace('\n', '').replace('\r', '')
+            result.append({
+                'page' : pno,
+                'x0' : block[0],
+                'y0' : block[1],
+                'x1' : block[2],
+                'y1' : block[3],
+                'text' : txt,
+                'block_type' : block[5],
+                'block_no' : block[6],
+            })
+            if not txt in list_string:
+                list_string.append(txt)
+        pno += 1
+
+    string_path = '{}.csv'.format(input_path)
+    with open(string_path, 'w', encoding='utf8', newline="") as fp:
+        writer = csv.writer(fp)
+        for s in list_string:
+            writer.writerow([s, 'todo'])
+    data = {
+        "input_path" : input_path,
+        "string_path" : string_path,
+        "text_block" : result
+    }
+    with open('{}.json'.format(input_path), mode='w', encoding='utf8') as fp:
+        json.dump(data, fp, sort_keys=True, indent=4, ensure_ascii=False)
+
+
+if __name__ == '__main__':
+    main(sys.argv)
---
+++ embed_annots.py
@@ -0,0 +1,41 @@
+import sys
+import fitz
+import csv
+import requests
+import json
+from tqdm import tqdm
+
+
+def main(argvs):
+    """メイン処理"""
+    argvs = sys.argv
+    argc = len(argvs)
+    if argc != 3:
+        print("Usage #python %s [JSONパス] [出力パス]" % argvs[0])
+        exit()
+    json_path = argvs[1]
+    out_path = argvs[2]
+    with open(json_path, mode='r', encoding='utf8') as fp:
+        json_info = json.load(fp)
+
+    string_path = json_info['string_path']
+    string_info = {}
+    with open(string_path, 'r', encoding='utf8', newline="\n") as fp:
+        reader = csv.reader(fp)
+        for row in reader:
+            string_info[row[0]] = row[1]
+    doc = fitz.open(json_info['input_path'])
+
+    for text_block in tqdm(json_info["text_block"]):
+        if text_block['text'] in string_info:
+            txt = string_info[text_block['text']]
+            page = doc[text_block['page']]
+            page.addTextAnnot(
+                (text_block['x1'] - 20, text_block['y1'] - 20),
+                txt
+            )
+
+    doc.save(out_path, garbage=1, clean=1, deflate=1) 
+
+if __name__ == '__main__':
+    main(sys.argv)
